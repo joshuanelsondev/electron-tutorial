987c92c7f48f697f3a21cfce5fc162b7e589f60b
joshuanelsondev <joshuanelsondev@gmail.com>
2025-01-28 23:41:52 -0500

    refactor: update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
